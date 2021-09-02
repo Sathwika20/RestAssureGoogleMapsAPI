a81453e193cd3bb7ca33fa5d3e5091353c560fd4
Sathwika20 <gopirajsathwika1999@gmail.com>
2021-09-02 14:19:14 +0530

    handling dynamic json values

---
+++ src/test/java/restAssure/apiTesting/DynamicJson.java
@@ -0,0 +1,32 @@
+package restAssure.apiTesting;
+
+import files.Payload;
+import files.ReusableMethods;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class DynamicJson {
+    @Test(dataProvider = "BooksData")
+    public void addBook(String isbn, String aisle){
+        RestAssured.baseURI = "http://216.10.245.166";
+        String response = given().log().all().header("Content-Type","application/json")
+                .body(Payload.addBook(isbn,aisle))
+                .when()
+                .post("/Library/Addbook.php")
+                .then().log().all().assertThat().statusCode(200)
+                .extract().response().asString();
+        JsonPath js = ReusableMethods.rawToJson(response);
+        String id = js.get("ID");
+        System.out.println(id);
+
+    }
+    //parameterised testing with multiple data sets
+    @DataProvider(name = "BooksData")
+    public Object[][] getData(){
+        return new Object[][] {{"jsxnk","8765"},{"skjdh","9876"},{"sakjjh","98765"}};
+    }
+}
